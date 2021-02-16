a7f25d904a259aab336bc8caea537c7612258ec3
Laguna1 <lagunazoomarket@gmail.com>
2021-02-16 14:25:43 +0200

    Implement the ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,3 +1,40 @@
-const ButtonPanel = () => '';
+import Button from './Button';
+
+const ButtonPanel = () => (
+  <div>
+    <div>
+      <div>AC</div>
+      <div>+/-</div>
+      <div>%</div>
+    </div>
+
+    <div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>X</div>
+    </div>
+
+    <div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+    </div>
+
+    <div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>+</div>
+    </div>
+
+    <div>
+      <div>0</div>
+      <div>.</div>
+      <div>=</div>
+    </div>
+  </div>
+);
 
 export default ButtonPanel;
